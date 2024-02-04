4eb0d1aefa6e4bb89085d04235df8754ecaa11c7
Yuto Hashimoto <69133317+hashimo1216@users.noreply.github.com>
2024-02-04 18:22:34 +0900

    make navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   const [openMenu, setOpenMenu] = useState<boolean>(false);
   return (
     <div>
-      <nav className="h-[80px] px-[40px] w-full flex justify-between fixed items-center bg-blue-200">
+      <nav className="h-[80px] px-[40px] w-full flex justify-between fixed items-center bg-white">
         <h1 className="font-serif">
           <Link href="/" className="text-4xl">
             Sneakers
@@ -15,30 +15,32 @@ export default function Navbar() {
         </h1>
         <button
           onClick={() => setOpenMenu(!openMenu)}
-          className="h-[60px] text-white flex flex-col gap-1 justify-center items-center"
+          className={`h-[60px] flex flex-col gap-1 justify-center items-center z-40`}
         >
           <span
-            className={`bg-gray-500 block transition-all duration-300 ease-out h-0.5 w-8 rounded-sm ${openMenu ? "rotate-225 translate-y-1" : ""}`}
+            className={`block bg-gray-600 transition-all duration-300 ease-out h-0.5 w-8 rounded-sm ${openMenu ? "rotate-225 translate-y-1 bg-white" : ""}`}
           ></span>
           <span
-            className={`bg-gray-500 block transition-all duration-300 ease-out 
+            className={`block bg-gray-600 transition-all duration-700 ease-out 
 h-0.5 w-8 rounded-sm my-0.5 ${openMenu ? "hidden" : ""}`}
           ></span>
           <span
-            className={`bg-gray-500 block transition-all duration-300 ease-out 
-h-0.5 w-8 rounded-sm ${openMenu ? "-rotate-225 -translate-y-0.5" : ""}`}
+            className={`block bg-gray-600 transition-all duration-300 ease-out h-0.5 w-8 rounded-sm ${openMenu ? "-rotate-225 -translate-y-0.5 bg-white" : ""}`}
           ></span>
         </button>
+        {openMenu && (
+          <div className="h-screen w-full fixed bg-black opacity-80"></div>
+        )}
       </nav>
       <div
-        className={`h-screen w-64 text-white bg-gray-900 relative duration-300 ${openMenu ? "" : "-translate-x-64"}`}
+        className={`h-screen w-[300px] px-6 text-white bg-black fixed z-10 duration-500 ${openMenu ? "" : "-translate-x-64 opacity-0"}`}
       >
-        <ul>
-          <li>PICK UP</li>
-          <li>FEATURE</li>
-          <li>CONTACT</li>
+        <ul className="py-[60px]">
+          <li className="py-[20px] border-t-[1px]">PICK UP</li>
+          <li className="py-[20px] border-y-[1px]">FEATURE</li>
+          <li className="py-[20px] border-b-[1px]">CONTACT</li>
         </ul>
-        <ul>
+        <ul className="py-2 flex flex-col gap-3 text-[15px]">
           <li>Twitter</li>
           <li>facebook</li>
           <li>instagram</li>
